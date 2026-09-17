dbbd3b3808b6452e89ecadf4e1a3ae6695399fd8
Harith Javed Bakhrani <harithjaved@gmail.com>
2026-09-17 12:01:36 +0300

    Add destination Google Maps links to booking popup

---
+++ config/locations.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+// Destination locations supplied by the site owner.
+return [
+    'sahajanand' => 'https://maps.app.goo.gl/PKZZCN5N8qjRS4PS7',
+    'feeding' => 'https://maps.app.goo.gl/phauXQRgXL2CShGK6',
+    'galana' => 'https://maps.app.goo.gl/nGtmkZuDLRiNLKAz7',
+];
