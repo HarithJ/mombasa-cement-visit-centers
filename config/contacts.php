8ec0341f1fbee5c59dcfc48383719372f874f20b
Harith Javed Bakhrani <harithjaved@gmail.com>
2026-09-22 11:28:58 +0300

    Show destination contacts after booking confirmation

---
+++ config/contacts.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+return [
+    'sahajanand' => [
+        'name' => 'Sahajanand Special School',
+        'people' => [
+            ['name' => '', 'phone' => '0723074477', 'tel' => '+254723074477', 'email' => 'Prafulmukhi1065@gmail.com'],
+            ['name' => '', 'phone' => '0712987530', 'tel' => '+254712987530', 'email' => 'Maryangie237@gmail.com'],
+            ['name' => '', 'phone' => '0793955001', 'tel' => '+254793955001', 'email' => 'Aslam.shakur1@gmail.com'],
+        ],
+    ],
+    'galana' => [
+        'name' => 'Galana Farm',
+        'people' => [
+            ['name' => 'Jaco', 'phone' => '0706 093132', 'tel' => '+254706093132', 'email' => 'jaco@nyumbagri.com'],
+            ['name' => 'Pravin', 'phone' => '+254 790 225592', 'tel' => '+254790225592', 'email' => 'pravin@nyumbagri.com'],
+        ],
+    ],
+    'feeding' => [
+        'name' => 'Kibarani Feeding Center',
+        'people' => [
+            ['name' => 'Gulam', 'phone' => '0703 170040', 'tel' => '+254703170040', 'email' => 'ghulamsalim@yahoo.com'],
+            ['name' => 'Karthik', 'phone' => '+254 719 669685', 'tel' => '+254719669685', 'email' => 'karthik@nyumba.com'],
+        ],
+    ],
+];
