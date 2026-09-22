7587569adaa13a0c100782ac2f69e93d5b6d53cc
Harith Javed Bakhrani <harithjaved@gmail.com>
2026-09-22 10:26:11 +0300

    Offer hourly destination visit slots

--- config/destinations.php
+++ config/destinations.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 return [
-    'sahajanand' => ['name' => 'Sahajanand Special School', 'slots' => ['09:00', '11:00', '14:00']],
-    'galana' => ['name' => 'Galana Farm', 'slots' => ['09:00', '11:00', '14:00']],
+    'sahajanand' => ['name' => 'Sahajanand Special School', 'slots' => ['09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00']],
+    'galana' => ['name' => 'Galana Farm', 'slots' => ['09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00']],
     'feeding' => ['name' => 'Kibarani Feeding center', 'slots' => ['11:00']],
 ];
